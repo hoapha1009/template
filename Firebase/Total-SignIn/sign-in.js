3e0c497178fb6c1857457917ae900120b30f5152
hoapha1009 <hoapha1009@gmail.com>
2021-02-23 20:55:25 +0700

    Sign in

---
+++ Firebase/Total-SignIn/sign-in.js
@@ -0,0 +1,14 @@
+// trong component sign in
+
+import {
+    auth,
+    createUserProfileDocument,
+} from "../Firestore/setup-firestore/setup-firestore";
+
+// ở form submit: async
+try {
+    // đăng nhập user vào firebase
+    await auth.signInWithEmailAndPassword(email, password);
+} catch (error) {
+    console.log(error);
+}
